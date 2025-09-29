6919e51ea7445cb13dd81e8daabd1fecfafc2ebf
Maga222006 <maga222006@mail.ru>
2025-09-29 02:01:39 +0200

    MultiagentPersonalAssistant

--- agents/models.py
+++ agents/models.py
@@ -1,19 +1,28 @@
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
+from openai import AsyncOpenAI
+import os
 
 load_dotenv()
 
+groq_client = AsyncOpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
 llm_supervisor = init_chat_model(
-    "groq:openai/gpt-oss-20b",
+    model="groq:openai/gpt-oss-120b",
     max_tokens=1000
 )
 
 llm_peripheral = init_chat_model(
-    "groq:gemma2-9b-it",
-    max_tokens=4000
+   model="groq:gemma2-9b-it"
 )
 
 llm_sub_agents = init_chat_model(
-    "groq:qwen/qwen3-32b",
-    max_tokens=3000
+    model="groq:qwen/qwen3-32b"
+)
+
+llm_image = init_chat_model(
+    model="groq:meta-llama/llama-4-scout-17b-16e-instruct"
 )
--- agents/utils/file_preprocessing.py
+++ agents/utils/file_preprocessing.py
@@ -21,7 +21,7 @@ async def preprocess_file(file_name: str):
 
 
 async def preprocess_audio(file_name):
-    from agent.models import groq_client
+    from agents.models import groq_client
     transcription = await groq_client.audio.transcriptions.create(
         model="whisper-large-v3-turbo",
         file=open(file_name, "rb")
@@ -30,7 +30,7 @@ async def preprocess_audio(file_name):
 
 
 async def preprocess_image(file_name: str):
-    from agent.models import llm_image
+    from agents.models import llm_image
     with open(file_name, "rb") as f:
         img_b64 = base64.b64encode(f.read()).decode("utf-8")
     response = await llm_image.ainvoke([HumanMessage(
